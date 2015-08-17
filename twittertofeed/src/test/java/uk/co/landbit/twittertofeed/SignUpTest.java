b01248c3636f43c18df532e599a81ff8649e59c5
Laurent Kéléchian. <laurent.kelechian@landbit.co.uk>
2015-08-17 15:20:12 +0100

    Initial unit tests

---
+++ twittertofeed/src/test/java/uk/co/landbit/twittertofeed/SignUpTest.java
@@ -0,0 +1,62 @@
+package uk.co.landbit.twittertofeed;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+import java.nio.charset.Charset;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("scratch")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TwitterToFeedApplication.class)
+@WebAppConfiguration
+public class SignUpTest {
+
+    private static Logger LOG = LoggerFactory.getLogger(SignUpTest.class);
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+	    MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private WebApplicationContext context;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void shouldRedirectToSignupPage() throws Exception {
+	LOG.debug("Get /signup");
+        this.mockMvc
+        	.perform(get("/signup"))
+        	.andExpect(redirectedUrl("/user/signup"))
+//        	.andExpect(status().is3xxRedirection())
+//        	.andExpect(content().contentType(contentType))
+        	;
+    }
+    
+}
